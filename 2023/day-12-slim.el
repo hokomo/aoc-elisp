1e95de1d3237b1ae9e95d65f37296393ebbe0214
hokomo <hokomo@disroot.org>
2023-12-13 23:22:09 +0100

    2023-12: Done

--- 2023/day-12-slim.el
+++ 2023/day-12-slim.el
@@ -5,53 +5,37 @@
 (defun read-12 (string)
   (-map #'parse-record (s-split "\n" string t)))
 
-(defun range-rle (record range)
-  (-let (((beg . end) range)
-         (prev nil)
-         (count 0)
-         (rle '()))
-    (cl-loop for i from beg below end
-             for c = (v. record i)
-             do (if (or (not prev) (= c prev))
-                    (cl-incf count)
-                  (push (cons prev count) rle)
-                  (setf count 1))
-                (setf prev c))
-    (unless (zerop count)
-      (push (cons prev count) rle))
-    (nreverse rle)))
+(defalias 'count-arrangements
+  (memoize
+   (lambda (list groups)
+     (let ((list (--drop-while (= it ?.) list)))
+       (cond
+        ((not list)
+         (int (not groups)))
+        ((not groups)
+         (int (--all-p (/= it ?#) list)))
+        (t
+         (let ((c (car list))
+               (len (car groups)))
+           (+
+            (seq-let [prefix suffix] (-split-at len list)
+              (if (and (= (length prefix) len)
+                       (--all-p (/= it ?.) prefix)
+                       (not (equal (car suffix) ?#)))
+                  (count-arrangements (cdr suffix) (cdr groups))
+                0))
+            (if (= c ??)
+                (count-arrangements (cdr list) groups)
+              0)))))))))
 
-(defun record-ranges (record)
-  (->> (s-matched-positions-all (rx (+ (any "?#"))) record)
-       (--map (range-rle record it))))
-
-(defun range-free-p (range)
-  (and (= (length range) 1) (= (caar range) ??)))
-
-(defun count-arrangements (record lengths)
-  (named-let rec ((ranges (record-ranges record))
-                  (lengths lengths))
-    (if (not lengths)
-        (and (--all-p (range-free-p it) ranges) 1)
-      (-let* (((range . ranges) ranges)
-              ((length . lengths) lengths)
-              (((char . count) . spans) range))
-        (cond
-         ((and (not spans) (= char ?#))
-          (and (= count length) (rec ranges lengths)))
-         ((and (not spans) (= char ??))
-          (if (> length count)
-              (rec ranges (cons length lengths))
-            (+ ;; There are count - length possible placements where the group
-               (cl-loop for i from 0 below (- count length)
-                        sum (rec `(((?? . ,(- count length i 1))) ,@ranges)
-                                 lengths))
-               (rec ranges lengths))))
-         (t
-          ))))))
+(defun prepare-record (record repetitions)
+  (seq-let [string groups] record
+    (let ((list (listify string)))
+      (list (apply #'append list (-repeat (1- repetitions) (cons ?? list)))
+            (apply #'append (-repeat repetitions groups))))))
 
 (defun solve-12-1 (records)
-  )
+  (-sum (--map (apply #'count-arrangements (prepare-record it 1)) records)))
 
 (defun solve-12-2 (records)
-  )
+  (-sum (--map (apply #'count-arrangements (prepare-record it 5)) records)))
--- 2023/day-12.el
+++ 2023/day-12.el
@@ -1,26 +1,15 @@
 ;; -*- lexical-binding: t; aoc-save-slim-p: t; -*-
 
 (require 'aoc-util)
-(require 'cl-lib)
 (require 'dash)
 (require 's)
 
-;;; We use a backtracking approach with memoization.
+;;; For both parts, we use a dynamic programming approach with memoization. Our
+;;; state consists of the remaining list of characters and the remaining list of
+;;; groups to assign.
 ;;;
-;;; Each record is represented as a sequence of contiguous ranges containing
-;;; only `#' or `?', delimited by one or more `.'. Each range is further
-;;; represented using the run-length encoding, as a sequence of counts of each
-;;; character in the range.
-;;;
-;;; E.g. the record
-;;;
-;;;     ..###..???..??##???##?..
-;;;
-;;; is represented as the list
-;;;
-;;;    (((?# . 3))
-;;;     ((?? . 3))
-;;;     ((?? . 2) (?# . 2) (?? . 3) (?# . 2) (?? . 1)))
+;;; We convert the strings to lists for easier memoization and structure
+;;; sharing.
 
 (defun parse-record (line)
   (seq-let [springs damaged] (s-split " " line t)
@@ -39,77 +28,52 @@
 
 (definput *input-12* #'read-12 "day-12-input.txt")
 
-(defun range-rle (record range)
-  (-let (((beg . end) range)
-         (prev nil)
-         (count 0)
-         (rle '()))
-    (cl-loop for i from beg below end
-             for c = (v. record i)
-             do (if (or (not prev) (= c prev))
-                    (cl-incf count)
-                  (push (cons prev count) rle)
-                  (setf count 1))
-                (setf prev c))
-    (unless (zerop count)
-      (push (cons prev count) rle))
-    (nreverse rle)))
+(defalias 'count-arrangements
+  (memoize
+   (lambda (list groups)
+     (let ((list (--drop-while (= it ?.) list)))
+       (cond
+        ;; The list is done, so the groups must be as well.
+        ((not list)
+         (int (not groups)))
+        ;; The groups are done, so the rest must not be `#'s.
+        ((not groups)
+         (int (--all-p (/= it ?#) list)))
+        ;; Otherwise, inspect the current non-`.' character.
+        (t
+         (let ((c (car list))
+               (len (car groups)))
+           (+
+            ;; A group could start at this position, whether because we decided
+            ;; so or are forced to do so (i.e. `c' is a `?' or a `#').
+            (seq-let [prefix suffix] (-split-at len list)
+              (if (and (= (length prefix) len)
+                       (--all-p (/= it ?.) prefix)
+                       ;; Account for a `.' at the end of a group. We use
+                       ;; `equal' to conveniently handle the case when
+                       ;; `suffix' is nil.
+                       (not (equal (car suffix) ?#)))
+                  (count-arrangements (cdr suffix) (cdr groups))
+                0))
+            ;; Or, we could decide to put a `.' at this position.
+            (if (= c ??)
+                (count-arrangements (cdr list) groups)
+              0)))))))))
 
-(defun record-ranges (record)
-  (->> (s-matched-positions-all (rx (+ (any "?#"))) record)
-       (--map (range-rle record it))))
-
-(defun range-free-p (range)
-  (and (= (length range) 1) (= (caar range) ??)))
-
-(defun count-arrangements (record lengths)
-  (named-let rec ((ranges (record-ranges record))
-                  (lengths lengths))
-    (if (not lengths)
-        (and (--all-p (range-free-p it) ranges) 1)
-      (-let* (((range . ranges) ranges)
-              ((length . lengths) lengths)
-              (((char . count) . spans) range))
-        (cond
-         ;; A complete range (all `#'s). The current group *must* fit here.
-         ((and (not spans) (= char ?#))
-          (and (= count length) (rec ranges lengths)))
-         ;; A free range (all `?'). The current group *could* fit here.
-         ((and (not spans) (= char ??))
-          (if (> length count)
-              ;; If the group doesn't fit, we have no choice but to move on
-              ;; (effectively assigning all `.'s to this range).
-              (rec ranges (cons length lengths))
-            ;; Otherwise, the group can be placed at many different starting
-            ;; positions. Every `?' to the left of the placement is assigned to
-            ;; `.', but all `?' to the right could be used by upcoming groups.
-            (+ ;; There are count - length possible placements where the group
-               ;; does not touch the end of the range, which means we need to
-               ;; make sure to properly delimit the the group with an ending
-               ;; `.'. Therefore there are count - length - i - 1 `?'s
-               ;; remaining.
-               (cl-loop for i from 0 below (- count length)
-                        sum (rec `(((?? . ,(- count length i 1))) ,@ranges)
-                                 lengths))
-               ;; If the group is placed at the end, delimiting it is not
-               ;; necessary because ranges are guaranteed to be contiguous and
-               ;; not touch.
-               (rec ranges lengths))))
-         ;; A mixed range (both `#' and `?'). The current group *could* fit in a
-         ;; part of this range, in which case we also need an explicit `.' to
-         ;; mark its end.
-         (t
-          ;; TODO
-          ))))))
+(defun prepare-record (record repetitions)
+  (seq-let [string groups] record
+    (let ((list (listify string)))
+      (list (apply #'append list (-repeat (1- repetitions) (cons ?? list)))
+            (apply #'append (-repeat repetitions groups))))))
 
 (defun solve-12-1 (records)
-  )
+  (-sum (--map (apply #'count-arrangements (prepare-record it 1)) records)))
 
-(expect (solve-12-1 *test-12*) nil)
-(expect (solve-12-1 *input-12*) nil)
+(expect (solve-12-1 *test-12*) 21)
+(expect (solve-12-1 *input-12*) 7732)
 
 (defun solve-12-2 (records)
-  )
+  (-sum (--map (apply #'count-arrangements (prepare-record it 5)) records)))
 
-(expect (solve-12-2 *test-12*) nil)
-(expect (solve-12-2 *input-12*) nil)
+(expect (solve-12-2 *test-12*) 525152)
+(expect (solve-12-2 *input-12*) 4500070301581)
