6ccca9dfb918ef4d5d1a639786efc9af5d252031
hokomo <hokomo@disroot.org>
2023-12-16 22:51:45 +0100

    2023-16: Clean up

--- 2023/day-16-slim.el
+++ 2023/day-16-slim.el
@@ -1,7 +1,7 @@
 (defun read-16 (string)
   (vecify (s-split "\n" string t)))
 
-(defun beam-continue (tile dir)
+(defun beam-bounce (tile dir)
   (cl-ecase tile
     (?. (list dir))
     (?| (if (member dir '([-1 0] [1 0])) (list dir) `([-1 0] [1 0])))
@@ -12,42 +12,35 @@
 (defun beam-step (grid beam)
   (pcase-let ((dims (vdims grid))
               (`(,pos ,dir) beam))
-    (for ((ndir (beam-continue (v2.. grid pos) dir))
+    (for ((ndir (beam-bounce (v2.. grid pos) dir))
           (:let ((npos (v2+ pos ndir))))
           (:when (v2< [-1 -1] npos dims)))
       (list npos ndir))))
 
-(defun beam-simulate (grid init beamfn)
-  (pfor-do ((beams (:step init nbeams))
-            (:let ((nbeams '())))
-            (:while beams))
-    (dolist (beam beams)
-      (when (funcall beamfn beam)
-        (dolist (nbeam (beam-step grid beam))
-          (push nbeam nbeams))))))
-
-(defun solve-16-1 (grid)
-  (energize-for grid '(([0 0] [0 1]))))
-
-(defun energize-for (grid init)
+(defun beam-simulate (grid init)
   (let ((seen (st))
         (energized (st)))
-    (beam-simulate grid init
-                   (lambda (beam)
-                     (seq-let [pos _] beam
-                       (prog1 (not (s. seen beam))
-                         (setf (s. energized pos) t
-                               (s. seen beam) t)))))
+    (for-do ([(beams (:step (list init) nbeams))
+              (:let ((nbeams '())))
+              (:while beams)]
+             (beam beams)
+             (:when (not (s. seen beam)))
+             (:let ((`(,pos ,_) beam))))
+      (setf (s. seen beam) t
+            (s. energized pos) t)
+      (dolist (nbeam (beam-step grid beam))
+        (push nbeam nbeams)))
     (set-size energized)))
 
-(defun solve-16-2 (grid)
+(defun solve-16-1 (grid)
+  (beam-simulate grid '([0 0] [0 1])))
+
+(defun beams-edges (grid)
   (seq-let [h w] (vdims grid)
-    (-max (--map (energize-for grid (list it))
-                 (append (for ((r (:range h)))
-                           `([,r 0] [0 1]))
-                         (for ((r (:range h)))
-                           `([,r ,(1- w)] [0 -1]))
-                         (for ((c (:range w)))
-                           `([0 ,c] [1 0]))
-                         (for ((c (:range w)))
-                           `([,(1- h) ,c] [-1 0])))))))
+    (append (for ((r (:range h))) `([,r 0] [0 1]))
+            (for ((r (:range h))) `([,r ,(1- w)] [0 -1]))
+            (for ((c (:range w))) `([0 ,c] [1 0]))
+            (for ((c (:range w))) `([,(1- h) ,c] [-1 0])))))
+
+(defun solve-16-2 (grid)
+  (-max (-map (-partial #'beam-simulate grid) (beams-edges grid))))
--- 2023/day-16.el
+++ 2023/day-16.el
@@ -3,8 +3,6 @@
 (require 'aoc-util)
 (require 'cl-lib)
 (require 'dash)
-(require 'ht)
-(require 'queue)
 (require 's)
 
 (defun read-16 (string)
@@ -24,7 +22,7 @@
 
 (definput *input-16* #'read-16 "day-16-input.txt")
 
-(defun beam-continue (tile dir)
+(defun beam-bounce (tile dir)
   (cl-ecase tile
     (?. (list dir))
     (?| (if (member dir '([-1 0] [1 0])) (list dir) `([-1 0] [1 0])))
@@ -35,48 +33,41 @@
 (defun beam-step (grid beam)
   (pcase-let ((dims (vdims grid))
               (`(,pos ,dir) beam))
-    (for ((ndir (beam-continue (v2.. grid pos) dir))
+    (for ((ndir (beam-bounce (v2.. grid pos) dir))
           (:let ((npos (v2+ pos ndir))))
           (:when (v2< [-1 -1] npos dims)))
       (list npos ndir))))
 
-(defun beam-simulate (grid init beamfn)
-  (pfor-do ((beams (:step init nbeams))
-            (:let ((nbeams '())))
-            (:while beams))
-    (dolist (beam beams)
-      (when (funcall beamfn beam)
-        (dolist (nbeam (beam-step grid beam))
-          (push nbeam nbeams))))))
+(defun beam-simulate (grid init)
+  (let ((seen (st))
+        (energized (st)))
+    (for-do ([(beams (:step (list init) nbeams))
+              (:let ((nbeams '())))
+              (:while beams)]
+             (beam beams)
+             (:when (not (s. seen beam)))
+             (:let ((`(,pos ,_) beam))))
+      (setf (s. seen beam) t
+            (s. energized pos) t)
+      (dolist (nbeam (beam-step grid beam))
+        (push nbeam nbeams)))
+    (set-size energized)))
 
 (defun solve-16-1 (grid)
-  (energize-for grid '(([0 0] [0 1]))))
+  (beam-simulate grid '([0 0] [0 1])))
 
 (expect (solve-16-1 *test-16*) 46)
 (expect (solve-16-1 *input-16*) 7623)
 
-(defun energize-for (grid init)
-  (let ((seen (st))
-        (energized (st)))
-    (beam-simulate grid init
-                   (lambda (beam)
-                     (seq-let [pos _] beam
-                       (prog1 (not (s. seen beam))
-                         (setf (s. energized pos) t
-                               (s. seen beam) t)))))
-    (set-size energized)))
+(defun beams-edges (grid)
+  (seq-let [h w] (vdims grid)
+    (append (for ((r (:range h))) `([,r 0] [0 1]))
+            (for ((r (:range h))) `([,r ,(1- w)] [0 -1]))
+            (for ((c (:range w))) `([0 ,c] [1 0]))
+            (for ((c (:range w))) `([,(1- h) ,c] [-1 0])))))
 
 (defun solve-16-2 (grid)
-  (seq-let [h w] (vdims grid)
-    (-max (--map (energize-for grid (list it))
-                 (append (for ((r (:range h)))
-                           `([,r 0] [0 1]))
-                         (for ((r (:range h)))
-                           `([,r ,(1- w)] [0 -1]))
-                         (for ((c (:range w)))
-                           `([0 ,c] [1 0]))
-                         (for ((c (:range w)))
-                           `([,(1- h) ,c] [-1 0])))))))
+  (-max (-map (-partial #'beam-simulate grid) (beams-edges grid))))
 
 (expect (solve-16-2 *test-16*) 51)
 (expect (solve-16-2 *input-16*) 8244 :gc 4000000000)
